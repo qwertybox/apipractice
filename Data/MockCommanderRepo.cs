a1a8e9a20eac93bbc341505f3d7b98fcdf0f3675
Bohdan <ovsak.bohdan@gmail.com>
2021-07-04 17:24:53 +0300

    developing api

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>{
+                new Command {Id=0, HowTo="Do smt", Line="Done smt", Platform="Pen"},
+                new Command {Id=1, HowTo="Make a cup o tea", Line="Add sugar", Platform="sugar & tea"},
+                new Command {Id=2, HowTo="Cut beard", Line="Get a knife", Platform="Beard & knife"}
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command {Id=0, HowTo="Do smt", Line="Done smt", Platform="Pen"};
+        }
+    }
+}
